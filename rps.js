d4d3ff32c6d7cab9c4b99c2aeabe17c4666443c5
m_droubi <mohammaddroubi2001@hotmail.com>
2025-06-17 10:49:06 +0300

    i added ui

--- rps.js
+++ rps.js
@@ -1,19 +1,17 @@
-let rock =  'rock'
+
+let rock = 'rock'
 let paper = 'paper'
 let scissors = 'scissors'
-let gameNumber = 0
 let humanScore = 0
 let computerScore = 0
-
-function playGame() {
-
-
-
-
+const Body = document.querySelector("body")
+const gop = document.querySelector('#re');
+const txt = document.createElement("div");
+txt.classList.add("txt");
+Body.appendChild(txt);
+txt.style.whiteSpace = "pre-line"
 
 function getComputerChoice() {
-    
-
    let a = (Math.random()*3);
     if (a <= 1) {
         return 'rock'
@@ -22,50 +20,44 @@ function getComputerChoice() {
     } else {
         return 'scissors'
     }
-
-}
-
-
-
-
-function getHumanChoice() {
-   return prompt('what is your choice?')
-    
 }
 
 
 
 function playRound(humanChoice, computerChoice) {
-    let s = humanChoice.toLowerCase();
+    let s = humanChoice;
     let n = computerChoice;
-    if (s === computerChoice) {
-        console.log('no one won, drow!')
+    if (s === n) {
+        txt.textContent = `drow!
+                           Your score: ${humanScore}
+                           Computer score: ${computerScore}`;
     } else if (((s == rock) && (n == scissors)) || ((s == scissors) && (n == paper)) || ((s == paper) && (n == rock))) {
         (humanScore++)
-         console.log(s + ' beat ' + 'your ' + computerChoice)
+        txt.textContent = `Your ${s} beat the ${computerChoice}
+                           Your score: ${humanScore}
+                           Computer score: ${computerScore}`;
     } else if (((n == rock) && (s == scissors)) || ((n == scissors) && (s == paper)) || ((n == paper) && (s == rock))) {
         (computerScore++)
-        console.log(computerChoice + ' beat ' + 'your ' + s)
-    
-    } else {}
-
+        txt.textContent = `The ${computerChoice} beat your ${s}
+                           Your score: ${humanScore}
+                           Computer score: ${computerScore}`;
+    }
 } 
 
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+gop.addEventListener('click', (event) => {
+   let humanSelection = event.target.id;
+   const computerSelection = getComputerChoice();
+   getComputerChoice()
+   if(humanScore < 5 && computerScore < 5) {
+   playRound(humanSelection, computerSelection);
+   } else if(humanScore == 5) { 
+      txt.textContent = `you won`  
+   } else if(computerScore == 5) {
+      txt.textContent = `game over`
+   }
+});
 
-    if (++gameNumber < 5) {
-        return playGame()
-    } else if (humanScore === computerScore) {
-         console.log('drow')
-    } else if (humanScore > computerScore) {
-         console.log('congratulation! you won')
-    } else {
-         console.log('game over')
-    }
 
-}
-console.log(playGame())
+
